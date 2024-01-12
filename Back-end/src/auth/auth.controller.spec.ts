24a9cf27cdb0853096f64019410bce21ae82b2bd
Naol <naolgezahegne@gmail.com>
2024-01-12 23:31:21 +0300

    added unit and e2e testing

---
+++ Back-end/src/auth/auth.controller.spec.ts
@@ -0,0 +1,31 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UserService } from '../user/user.service';
+import { JwtService } from '@nestjs/jwt';
+import { getModelToken } from '@nestjs/mongoose'; 
+
+describe('AuthController', () => {
+  let controller: AuthController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AuthController],
+      providers: [
+        AuthService,
+        UserService,
+        JwtService,
+        {
+          provide: getModelToken('User'), 
+          useValue: {},
+        },
+      ],
+    }).compile();
+
+    controller = module.get<AuthController>(AuthController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
---
+++ Back-end/src/auth/auth.service.spec.ts
@@ -0,0 +1,29 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { UserService } from '../user/user.service';
+import { JwtService } from '@nestjs/jwt';
+import { getModelToken } from '@nestjs/mongoose'; 
+
+describe('AuthService', () => {
+  let service: AuthService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        UserService,
+        JwtService,
+        {
+          provide: getModelToken('User'), 
+          useValue: {}, 
+        },
+      ],
+    }).compile();
+
+    service = module.get<AuthService>(AuthService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
